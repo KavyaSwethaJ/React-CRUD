451dcc6bfdc05707f22e77d7cecdc74d62507290
KavyaSwethaJ <j.kavyaswetha@gmail.com>
2024-09-07 20:21:50 +0530

    Add database connection

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,18 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+
+dotenv.config();
 
 const app = express();
 
-app.listen(5000, () => {
-    console.log("Server started on port 5000 Hello World");
-})
-
-app.get('/', (req, res) => {
+app.get('/products', (req, res) => {
     res.send("Hello World");
 });
 
+app.listen(5000, () => {
+    connectDB();
+    console.log("Server started on port 5000");
+})
 //video paused at 11.05
+// MONG)_DB PWD: gTiKFSmr2IV6G1NM

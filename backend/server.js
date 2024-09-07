89bfa69501a2de316ccb82901d4bde09e48f89ba
KavyaSwethaJ <j.kavyaswetha@gmail.com>
2024-09-07 22:44:56 +0530

    Add product creation endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,49 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import { connectDB } from './config/db.js';
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
 
 dotenv.config();
 
 const app = express();
 
-app.get('/products', (req, res) => {
-    res.send("Hello World");
+app.use(express.json());
+    
+app.get("/products", (req, res) => {
+  res.send("Hello World");
+});
+
+app.post("/api/products", (req, res) => {
+  const product = req.body;
+
+  if (!product.name || !product.price || !product.image) {
+    return res.status(400).json({
+      success: false,
+      message: "Please provide all fields",
+    });
+  }
+
+  const newProduct = new Product(product);
+
+  try {
+    newProduct.save();
+    res.status(201).json({
+      success: true,
+      message: "Product created",
+    });
+  } catch (error) {
+    console.log(`error: ${error.message}`);
+    res.status(500).json({
+      success: false,
+      message: "Server Error",
+    });
+  }
 });
 
 app.listen(5000, () => {
-    connectDB();
-    console.log("Server started on port 5000");
-})
+  connectDB();
+  console.log("Server started on port 5000");
+});
+
 //video paused at 11.05
 // MONG)_DB PWD: gTiKFSmr2IV6G1NM

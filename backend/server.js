bd710fd1d7e85e81a70d1f653db61314559b6405
KavyaSwethaJ <j.kavyaswetha@gmail.com>
2024-09-08 15:53:00 +0530

    Add API endpoints for product CRUD operations

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,28 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import Product from "./models/product.model.js";
+import mongoose, { mongo } from "mongoose";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
-    
-app.get("/products", (req, res) => {
-  res.send("Hello World");
+
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json({
+      success: true,
+      data: products,
+    });
+  } catch (error) {
+    console.log(`error in fetching products: ${error.message}`);
+    res.status(500).json({
+      success: false,
+      message: "Server Error",
+    });
+  }
 });
 
 app.post("/api/products", (req, res) => {
@@ -32,10 +45,57 @@ app.post("/api/products", (req, res) => {
       message: "Product created",
     });
   } catch (error) {
-    console.log(`error: ${error.message}`);
+    console.log(`error in creating the product: ${error.message}`);
     res.status(500).json({
       success: false,
       message: "Server Error",
+    });
+  }
+});
+
+app.put("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    res.status(404).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(id, product, {
+      new: true,
+    });
+    res.status(200).json({
+      success: true,
+      message: "Product updated",
+      data: updatedProduct,
+    });
+  } catch (error) {
+    console.log(`error in updating the product: ${error.message}`);
+    res.status(500).json({
+      success: false,
+      message: "Server Error",
+    });
+  }
+});
+
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await Product.findByIdAndDelete(id);
+    res.status(200).json({
+      success: true,
+      message: "Product deleted",
+    });
+  } catch (error) {
+    console.log(`error in deleting the product: ${error.message}`);
+    res.status(404).json({
+      success: false,
+      message: "Product not found",
     });
   }
 });
@@ -45,5 +105,5 @@ app.listen(5000, () => {
   console.log("Server started on port 5000");
 });
 
-//video paused at 11.05
-// MONG)_DB PWD: gTiKFSmr2IV6G1NM
+//video paused at 32.55
+// MONGO_DB PWD: gTiKFSmr2IV6G1NM
